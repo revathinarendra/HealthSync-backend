3fefc1fa606a43fbaa60ac242e4dedbd8bb4f4cc
revathinarendra <150423477+revathinarendra@users.noreply.github.com>
2025-07-21 15:15:45 +0530

    build changed

---
+++ vercel_postinstall.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+echo "Installing requirements..."
+pip install --upgrade pip
+pip install -r requirements.txt
+
+echo "Running Django setup..."
+mkdir -p static
+python manage.py collectstatic --noinput
+python manage.py migrate
