0863b068576bf1d71874e1a6f5a838e684937777
revathinarendra <150423477+revathinarendra@users.noreply.github.com>
2025-07-21 14:44:51 +0530

    build

--- build_files.sh
+++ build_files.sh
@@ -1,33 +1,17 @@
-# #!/bin/bash
-
-# # Ensure pip is available
-# /usr/bin/python3.9 -m ensurepip --upgrade
-
-# # Upgrade pip
-# /usr/bin/python3.9 -m pip install --upgrade pip
-
-# # Install dependencies from requirements.txt
-# /usr/bin/python3.9 -m pip install -r requirements.txt
-
-# # Create static directory if it doesn't exist
-# mkdir -p /vercel/path0/static
-
-# # Collect static files
-# python3.9 manage.py collectstatic --noinput
-
-# # Apply database migrations
-# python3.9 manage.py makemigrations
-# python3.9 manage.py migrate
 #!/bin/bash
 
-# Python setup
-python -m ensurepip --upgrade
-python -m pip install --upgrade pip
-python -m pip install -r requirements.txt
+# Ensure pip is available
+python3.9 -m ensurepip --upgrade
 
-# Django setup
-python manage.py collectstatic --noinput
-python manage.py makemigrations
-python manage.py migrate
+# Upgrade pip
+python3.9 -m pip install --upgrade pip
 
+# Install dependencies from requirements.txt
+python3.9 -m pip install -r requirements.txt
 
+# Collect static files
+python3.9 manage.py collectstatic --noinput
+
+# Apply database migrations
+python3.9 manage.py makemigrations
+python3.9 manage.py migrate
