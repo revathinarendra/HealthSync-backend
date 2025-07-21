debf3345689b4691c8f7581a9e46ef17d8226ede
revathinarendra <150423477+revathinarendra@users.noreply.github.com>
2025-07-21 15:40:01 +0530

    cros

--- vercel_postinstall.sh
+++ vercel_postinstall.sh
@@ -6,6 +6,6 @@ pip install --upgrade pip
 pip install -r requirements.txt
 
 echo "Running Django setup..."
-mkdir -p static
+# mkdir -p static
 python manage.py collectstatic --noinput
 python manage.py migrate
